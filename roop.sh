9f98a9e2a455dadbf64c37d52c91b9d2b42739be
tubutubucorn <33154559+tubutubucorn@users.noreply.github.com>
2018-08-25 13:34:48 +0900

    fixed roop.sh

--- roop.sh
+++ roop.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-for i in 1 10 100 1000 10000 100000
+for i in 1 10 100 1000 10000 100000 1000000 10000000
 do
-    name = "{i}b.txt"
-    name2 = "{i}b_s.txt"
-    name3 = "{i}b_n.txt"
-    time ./INSTALL_SKIP/bin/yara data/sample5.rules data/$name > $name2
-    time ./INSTALL_NOSKIP/binyara data/sample5.rules data/$name > $name3
+    name=$i"b.txt"
+
+    echo $name >&1
+    time ./SKIP/bin/yara data/sample5.rules data/$name
+done
+
+for i in 1 10 100 1000 10000 100000 1000000 10000000
+do
+    name=$i"b.txt"
+
+    echo $name >&1
+    time ./NOSKIP/bin/yara data/sample5.rules data/$name
 done
